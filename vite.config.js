84626ba4861e4c83abad928f5e6c9d21d288cc57
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-27 21:19:32 +0800

    fix config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+      host: 'wrms-accreditation.projectorb.shop',
+      https : true,
+    },
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,

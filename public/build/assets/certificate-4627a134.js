81aa2a8d2bc8b6e0f064a74bb1b39ba684adc542
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-28 17:55:18 +0800

    build production

---
+++ public/build/assets/certificate-4627a134.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,a as c}from"./app-544d6b58.js";const o={};function r(a,n,s,i,_,f){return t(),c("div",null," Certificate ")}const l=e(o,[["render",r]]);export{l as default};
---
+++ public/build/assets/organizationindex2-88863287.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./app-544d6b58.js";const r={};function e(t,c){return null}const _=n(r,[["render",e]]);export{_ as default};

b494b0840e965a541d19a1f580a35235b68433b2
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-04-18 10:04:14 +0800

    change

--- public/build/assets/applicationrequirements-1bae356e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,a as n}from"./app-619bd0ce.js";const o={};function r(a,c,s,p,i,_){return t(),n("div",null," Application Requiremnets ")}const u=e(o,[["render",r]]);export{u as default};
--- public/build/assets/organizationindex2-d99b6f7d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,a as t}from"./app-619bd0ce.js";const n={};function a(r,c,s,_,i,p){return o(),t("div")}const f=e(n,[["render",a]]);export{f as default};

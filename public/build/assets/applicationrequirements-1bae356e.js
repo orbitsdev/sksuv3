efad2f07dbbf22da08443f78373b980921f29e4e
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-04-18 09:35:06 +0800

    chnage

---
+++ public/build/assets/applicationrequirements-1bae356e.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,a as n}from"./app-619bd0ce.js";const o={};function r(a,c,s,p,i,_){return t(),n("div",null," Application Requiremnets ")}const u=e(o,[["render",r]]);export{u as default};
---
+++ public/build/assets/dashboard-a8e773a0.js
@@ -0,0 +1,1 @@
+import{a}from"./adminlayout-02f5365f.js";import{_ as t,o as n,c as e,r as s}from"./app-619bd0ce.js";const r={components:{adminlayout:a}};function c(p,m,_,d,i,f){const o=s("adminlayout");return n(),e(o)}const $=t(r,[["render",c]]);export{$ as default};
